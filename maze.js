a46c16c771b4767427aa0fc6d8ddda343d233da2
Jossette101 <jossettehowell@gmail.com>
2016-10-14 22:54:59 -0500

    question2

--- maze.js
+++ maze.js
@@ -11,8 +11,31 @@ function MouseOver(){
      */
     
 window.onload = function(){
-	var bound = document.getElementById("boundary1");
+	/* var bound = document.getElementById("boundary1");
 	bound.addEventListener("mouseover", function(){
 		bound.style.backgroundColor = "red";//
+	}); */
+	
+	/*var boundaries = document.querySelector("div");
+	[].forEach.call(boundaries, function(div){
+		div.style.color = "red";
 	});
+	
+	
+	document.getElementsByClassName(".boundary").addEventListener("hover",function(){
+		boundaries.style.backgroundColor = "red";
+		*/
+	var boundaries = document.querySelectorAll(".boundary");
+	
+	for(var c = 0; c < boundaries.length; c++){
+		boundaries[c].onmouseover = function(){
+			for(var x = 0; x<boundaries.length; x++){
+				boundaries[x].style.backgroundColor = "red";
+			}
+			//break;
+		}
+	}
 }
+
+
+//$(".boundary").css("color":"red");
